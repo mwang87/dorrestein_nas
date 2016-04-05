c65173cfd258e8c6f4d45056b21813dd8a126f51
ccms local user <ccms@gnps.ucsd.edu>
2016-04-05 14:47:46 -0700

    adding thing

---
+++ backup_nas.sh
@@ -0,0 +1,18 @@
+#rsync -av --no-perms --no-owner --no-group  root@dorresteinnas01.ucsd.edu:/volume1/ ./volume1
+#rsync -av --no-perms --no-owner --no-group  root@dorresteinnas01.ucsd.edu:/volume2/ ./volume2
+
+
+
+rsync -av --no-perms --no-owner --no-group  root@dorresteinnas01.ucsd.edu:/volume1/LauraPaceFolder/ ./volume1/LauraPaceFolder/
+rsync -av --no-perms --no-owner --no-group  root@dorresteinnas01.ucsd.edu:/volume1/AlexFolder/ ./volume1/AlexFolder/
+rsync -av --no-perms --no-owner --no-group  root@dorresteinnas01.ucsd.edu:/volume1/NehaFolder/ ./volume1/NehaFolder/
+rsync -av --no-perms --no-owner --no-group  root@dorresteinnas01.ucsd.edu:/volume1/RobbieFolder/ ./volume1/RobbieFolder/
+#rsync -av --no-perms --no-owner --no-group  root@dorresteinnas01.ucsd.edu:/volume1/SharedFolder/ ./volume1/SharedFolder/
+rsync -av --no-perms --no-owner --no-group  root@dorresteinnas01.ucsd.edu:/volume1/VanessaFolder/ ./volume1/VanessaFolder/
+rsync -av --no-perms --no-owner --no-group  root@dorresteinnas01.ucsd.edu:/volume1/DonFolder/ ./volume1/DonFolder/
+rsync -av --no-perms --no-owner --no-group  root@dorresteinnas01.ucsd.edu:/volume1/ckapono/ ./volume1/ckapono/
+rsync -av --no-perms --no-owner --no-group  root@dorresteinnas01.ucsd.edu:/volume1/TalFolder/ ./volume1/TalFolder/
+
+rsync -av --no-perms --no-owner --no-group  root@dorresteinnas01.ucsd.edu:/volume2/LauraSanchez/ ./volume2/LauraSanchez/
+rsync -av --no-perms --no-owner --no-group  root@dorresteinnas01.ucsd.edu:/volume2/YaoFolder/ ./volume2/YaoFolder/
+rsync -av --no-perms --no-owner --no-group  root@dorresteinnas01.ucsd.edu:/volume2/MingFolder/ ./volume2/MingFolder/
